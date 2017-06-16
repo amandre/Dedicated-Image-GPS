aa1f50dd179c8389470f91c0ab0ee84ae041a649
amandre <andrzejamghar@gmail.com>
2017-06-16 22:47:40 +0200

    Logging panel created, refactoring needed

--- main.py
+++ main.py
@@ -47,38 +47,75 @@ class SignInPage(tk.Frame):
         self.L1.pack(anchor=tk.W, padx=(50, 0))
         self.E1 = tk.Entry(self)
         self.E1.pack(anchor=tk.E, padx=(0, 50))
-        imageBtn = tk.Button(self, text="Choose an image to log in", command=SignUpPage.getimage)
+        self.coords=[]
+        imageBtn = tk.Button(self, text="Choose an image to log in", command=self.getimage)
         imageBtn.pack(pady=(10,0))
         submitBtn = tk.Button(self, text="Sign In", command=self.processLogIn)
         submitBtn.pack(side=tk.BOTTOM, pady=(0, 50))
 
     def processLogIn(self):
-        username= self.E1.get()
-        userExists = db.credentials.find_one({"username": username})
-        if userExists:
-            #usercoords = userExists['coords']
+        self.username= self.E1.get()
+        userExists = db.credentials.find_one({"username": self.username})
+        if userExists and self.filename and self.E1.get():
+            N = 9
+            filestr = re.findall("[\-\_\w\d]*?\.[jpg|png|jpeg|bmp]+", self.filename)
+            filestr = ''.join(filestr)
+            path = self.filename.replace(filestr, "")
+            self.usercoords = userExists['coords']
             #if re.match...
-            logInSuccessful=1
-        if logInSuccessful:
-            tkMessageBox.showinfo("USER "+username+" LOGGED IN", "You have been successfully signed in")
+            self.cropndisplay(path, filestr, N, self.username)
         else:
+            tkMessageBox.showerror("INCORRECT OR EMPTY USERNAME", "Invalid username. Please try again.")
+
+
+    def submitPasswd(self):
+        if cmp(self.coords,self.usercoords)<0:
             tkMessageBox.showerror("INCORRECT LOG IN", "Invalid username or password. Please try again.")
+        else:
+            tkMessageBox.showinfo("USER "+self.username+" LOGGED IN", "You have been successfully signed in")
 
+    def getimage(self):
+        self.filename = askopenfilename()
 
-class SignUpPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        self.L1 = tk.Label(self, text="User Name")
-        self.L1.pack(anchor=tk.W, padx=(50, 0))
-        self.E1 = tk.Entry(self)
-        self.E1.pack(anchor=tk.E, padx=(0, 50))
-        imageBtn = tk.Button(self, text="Choose an image to create your password", command=self.getimage)
-        imageBtn.pack(pady=(10,0))
-        #TODO 2 - some minor GUI enhancements
-        submitBtn = tk.Button(self, text="Sign Up", command=self.processRegistration)
-        submitBtn.pack(side=tk.BOTTOM, pady=(0, 50))
+    def cropndisplay(self, path, inputval, N, directory):
+        im = Image.open(path+inputval)
+        w, h = im.size
+        desiredw=w/N
+        desiredh=h/N
+        k=1
+        for i in range(0, h, desiredh):
+            for j in range(0, w, desiredw):
+                try:
+                    os.makedirs(path+directory)
+                except WindowsError:
+                    pass
+                #print str(i)+" "+str(j)
+                box = (j, i, j+desiredw, i+desiredh)
+                o = im.crop(box)
+                o.save(path+directory+"/IMG-"+str(k)+".png", "PNG")
+                k += 1
+        self.win = tk.Toplevel()
+        self.win.geometry("440x480")
+        self.win.resizable(width=False, height=False)
+        self.win.title("Coordinates selection")
+        tkMessageBox.showinfo("SELECT IMAGES", "Please type your password scheme to log in")
+        imagesList = [f for f in os.listdir(path+directory)]
+        N = int(numpy.sqrt(len(imagesList)))
+        if N*N==len(imagesList):
+            imagesArray= numpy.reshape(imagesList, (N, N))
+        for row in range(0, len(imagesArray[0])):
+            for col in range(0, len(imagesArray[1])):
+                rec = imagesArray[row, col]
+                img = ImageTk.PhotoImage(Image.open(path+directory+"/"+rec).resize((40, 40)))
+                square = tk.Label(self.win, image=img)
+                square.image = img
+                square.grid(row=row, column=col)
+                #TODO 3 - sort elements in the array
+                square.bind('<Button-1>', self.toggleonclick)
+        fr = tk.Frame(self.win)
+        fr.grid(columnspan=N)
+        sendCoords = tk.Button(fr, text="LOG IN USING THIS SEQUENCE", command=self.submitPasswd)
+        sendCoords.grid()
 
     def toggleonclick(self,event):
         event.widget.focus_set()
@@ -92,11 +129,37 @@ class SignUpPage(tk.Frame):
         else:
             self.coords.append([row, col])
             caller.config(borderwidth=2, relief="solid")
-        #print self.coords
+
+class SignUpPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.L1 = tk.Label(self, text="User Name")
+        self.L1.pack(anchor=tk.W, padx=(50, 0))
+        self.E1 = tk.Entry(self)
+        self.E1.pack(anchor=tk.E, padx=(0, 50))
+        self.coords = []
+        imageBtn = tk.Button(self, text="Choose an image to create your password", command=self.getimage)
+        imageBtn.pack(pady=(10, 0))
+        #TODO 2 - some minor GUI enhancements
+        submitBtn = tk.Button(self, text="Sign Up", command=self.processRegistration)
+        submitBtn.pack(side=tk.BOTTOM, pady=(0, 50))
+
+
+    def toggleonclick(self,event):
+        event.widget.focus_set()
+        caller = event.widget
+        grid_info = caller.grid_info()
+        row = grid_info["row"]
+        col = grid_info["column"]
+        if caller.cget("borderwidth")==2:
+            self.coords.remove([row, col])
+            caller.config(borderwidth=0)
+        else:
+            self.coords.append([row, col])
+            caller.config(borderwidth=2, relief="solid")
 
     def submitPasswd(self):
-        #print self.choice
-        #print self.coords
         self.win.destroy()
         username = self.E1.get()
         db.credentials.insert_one({
@@ -111,9 +174,6 @@ class SignUpPage(tk.Frame):
         self.win.geometry("440x480")
         self.win.resizable(width=False, height=False)
         self.win.title("Coordinates selection")
-
-        #L = tk.Label(self.win, text=txt)
-        #L.pack(side=tk.TOP,pady=(20,20))
         tkMessageBox.showinfo("SELECT IMAGES", "Please type your password scheme\nYou need to remember the sequence of selected images\nUnselected images will be excluded from the password sequence")
         imagesList = [f for f in os.listdir(path)]
         N = int(numpy.sqrt(len(imagesList)))
@@ -136,7 +196,6 @@ class SignUpPage(tk.Frame):
         sendCoords = tk.Button(fr, text="CREATE ACCOUNT", command=self.submitPasswd)
         sendCoords.grid()
 
-    @classmethod
     def crop(self, path, inputval, N, directory):
         im = Image.open(path+inputval)
         w, h = im.size
@@ -174,7 +233,7 @@ class SignUpPage(tk.Frame):
         else:
             tkMessageBox.showinfo("NO USERNAME OR EMPTY IMAGE", "Please try again.\nPut your desired username and the image to create password")
 
-    @classmethod
+    #@classmethod
     def getimage(self):
         self.filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
 
